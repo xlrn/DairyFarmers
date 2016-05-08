b2234b9ef9690705dd94c4a5cef5dbf70ec454a2
xlrn <ncl.yim@gmail.com>
2016-05-07 17:06:49 -0700

    Added saving, loading and cow theme

--- main.js
+++ main.js
@@ -5,20 +5,39 @@ function milkClick(number){
 	document.getElementById("milks").innerHTML = MilkBottles;
 };
 
-var Upgrades = 0;
+var Cows = 0;
 
-function buyUpgrade(){
-	var upgradeCost = Math.floor(10*Math.pow(1.1, Upgrades));
-	if (MilkBottles > upgradeCost) {
-		Upgrades += 1;
-		MilkBottles -= upgradeCost;
+function buyCow(){
+	var CowCost = Math.floor(10*Math.pow(1.1, Cows));
+	if (MilkBottles >= CowCost) {
+		Cows += 1;
+		MilkBottles -= CowCost;
 		document.getElementById('milks').innerHTML = MilkBottles;
-		document.getElementById('upgrades').innerHTML = Upgrades;
+		document.getElementById('Cows').innerHTML = Cows;
 	};
-	var nextCost = Math.floor(10*Math.pow(1.1, Upgrades));
+	var nextCost = Math.floor(10*Math.pow(1.1, Cows));
 	document.getElementById('cost').innerHTML = nextCost;
 };
 
 window.setInterval(function() {
-	milkClick(Upgrades);
+	milkClick(Cows);
 }, 1000);
+
+
+function save(){
+	var save = {
+		MilkBottles: MilkBottles,
+		Cows: Cows
+	}
+	localStorage.setItem("save",JSON.stringify(save));
+}
+
+function load(){
+	var savegame = JSON.parse(localStorage.getItem("save"));
+	if (typeof savegame.MilkBottles !== "undefined") MilkBottles = savegame.MilkBottles;
+	if (typeof savegame.Cows !== "undefined") Cows = savegame.Cows;
+}
+
+function deleteSave(){
+	localStorage.removeItem("save");
+}
