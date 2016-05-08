0d2bc6645be3588ac2c22294b2ff934eb8d5e268
xlrn <ncl.yim@gmail.com>
2016-05-07 17:10:17 -0700

    Added rounding to numbers

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ function buyCow(){
 	if (MilkBottles >= CowCost) {
 		Cows += 1;
 		MilkBottles -= CowCost;
-		document.getElementById('milks').innerHTML = MilkBottles;
-		document.getElementById('Cows').innerHTML = Cows;
+		document.getElementById('milks').innerHTML = prettify(MilkBottles);
+		document.getElementById('Cows').innerHTML = prettify(Cows);
 	};
 	var nextCost = Math.floor(10*Math.pow(1.1, Cows));
-	document.getElementById('cost').innerHTML = nextCost;
+	document.getElementById('cost').innerHTML = prettify(nextCost);
 };
 
 window.setInterval(function() {
@@ -40,4 +40,9 @@ function load(){
 
 function deleteSave(){
 	localStorage.removeItem("save");
+}
+
+function prettify(input){
+    var output = Math.round(input * 1000000)/1000000;
+	return output;
 }
